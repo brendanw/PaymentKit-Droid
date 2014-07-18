601208db12ee749ed75d44c3aa6d390fc131c651
Chris Pierick <chris@twotoasters.com>
2014-07-18 15:18:12 -0400

    Removed bad import of inputStyle to PKFragment

--- pKExample/src/main/java/com/brendan/pkexample/PKFragment.java
+++ pKExample/src/main/java/com/brendan/pkexample/PKFragment.java
@@ -1,6 +1,5 @@
 package com.brendan.pkexample;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,7 +12,6 @@ import android.widget.ImageView;
 import com.paymentkit.util.ToastUtils;
 import com.paymentkit.util.ViewUtils;
 import com.paymentkit.views.FieldHolder;
-import com.paymentkit.views.FieldHolder.InputStyle;
 
 /**
  * 
@@ -55,9 +53,6 @@ public class PKFragment extends Fragment {
 		float marginLeft = 1.0f - INPUT_WIDTH;
 		ViewUtils.setMarginLeft(mAcceptedCardsImg, (int) (marginLeft * ViewUtils.getScreenWidth(getActivity())));
 		ViewUtils.setWidth(mFieldHolder, (int) (INPUT_WIDTH * ViewUtils.getScreenWidth(getActivity())));
-		if(Build.VERSION.SDK_INT < 14) {
-			mFieldHolder.setInputStyle(InputStyle.GINGERBREAD);
-		}
 	}
 
 	/* After onCreateView is called */
